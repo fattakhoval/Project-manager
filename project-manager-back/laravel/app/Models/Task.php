9657f7b28e1a7790c69b3fb032e89e7ecb6e33b9
fattakhoval <fattahoval66@gmail.com>
2024-09-30 18:12:50 +0300

    созданы миграции
    созданы миграции, добавлены связи, и что-то еще

---
+++ project-manager-back/laravel/app/Models/Task.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+use App\Models\Project;
+
+class Task extends Model
+{
+    protected $fillable = ['title', 'description', 'projects_id', 'user_id',
+            'priority', 'date_start', 'date_end', 'status'];
+
+    public function project(){
+        return $this->belongsTo(Project::class);
+    }
+}
--- project-manager-back/laravel/database/migrations/2014_10_12_000000_create_users_table.php
+++ project-manager-back/laravel/database/migrations/2014_10_12_000000_create_users_table.php
@@ -17,6 +17,7 @@ return new class extends Migration
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->enum('role', ['Администратор','Руководитель','Исполнитель']);
             $table->rememberToken();
             $table->timestamps();
         });
